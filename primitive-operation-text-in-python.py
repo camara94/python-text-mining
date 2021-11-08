6e70701b5658aab6058df0f9218164fd4064dca7
Laby Damaro Camara <ldamaro98@gmail.com>
2021-11-08 04:48:09 +0100

    primitive contructs in text

---
+++ primitive-operation-text-in-python.py
@@ -0,0 +1,11 @@
+text1 = "Ethics are built right into the ideals and objectives of the United Nations "
+# the len of sentences
+print(len(text1))
+
+# tokenization that mean I converted the sentence into array of word
+text2 = text1.split(' ')
+
+# The len of array
+print(len(text2))
+# I print the array here
+print(text2)
